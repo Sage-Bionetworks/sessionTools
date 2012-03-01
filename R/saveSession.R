4da5980d667f28e4e038eec93c9c3b621e3c11a8
MattNapsAlot <matt.furia@sagebase.org>
2012-03-01 15:15:31 -0800

    Modifications to source file headers. Added funding acknoledgement and license verbage. Changed to GPL-3.

--- R/saveSession.R
+++ R/saveSession.R
@@ -1,8 +1,21 @@
-## Copyright 2012 Sage Bionetworks.
+## filename: saveSession.R
+## description: Save the current session.
 ##
-## Save the current session
-## 
-## Author: Matthew D. Furia <matt.furia@sagebase.org>
+## Funding Acknowledgement: 
+## The development of this software was supported by NCI Integrative Cancer 
+## Biology Program grant CA149237 and Washington State Life Science Discovery 
+## Fund Program Grant 3104672 to Sage Bionetworks <www.sagebase.org>.
+##
+## Copyright (C) 2012  Matthew D. Furia <matt.furia@sagebase.org>
+## This program is free software: providing the above funding acknolegement is
+## maintained, you may redistribute and/or modify it under the terms of the 
+## GNU General Public License as published by the Free Software Foundation, 
+## either version 3 of the License, or (at your option) any later version.
+##  
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 saveSession <- 
--- inst/integrationTests/test_isPackageAvailable.R
+++ inst/integrationTests/test_isPackageAvailable.R
@@ -1,8 +1,22 @@
-## Copyright 2012 Sage Bionetworks.
+## filename: test_isPackageAvailable.R
+## description: unit tests for package availability function. Run against a 
+## remote CRAN mirror.
 ##
-## unit tests for package availability function. Run agaings a remote CRAN mirror
-## 
-## Author: Matthew D. Furia <matt.furia@sagebase.org>
+## Funding Acknowledgement: 
+## The development of this software was supported by NCI Integrative Cancer 
+## Biology Program grant CA149237 and Washington State Life Science Discovery 
+## Fund Program Grant 3104672 to Sage Bionetworks <www.sagebase.org>.
+##
+## Copyright (C) 2012  Matthew D. Furia <matt.furia@sagebase.org>
+## This program is free software: providing the above funding acknolegement is
+## maintained, you may redistribute and/or modify it under the terms of the 
+## GNU General Public License as published by the Free Software Foundation, 
+## either version 3 of the License, or (at your option) any later version.
+##  
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 integrationTestSinglePackageAvail <-
