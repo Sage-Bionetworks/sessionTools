bf083cf144e3b03ae45c74d10a2145ce1d0575dd
Matthew D. Furia <matt.furia@sagebase.org>
2012-05-19 15:11:11 -0700

    fixed header of restoreClassDefinitions.R

--- R/restoreClassDefinitions.R
+++ R/restoreClassDefinitions.R
@@ -1,6 +1,20 @@
-# TODO: Add comment
-# 
-# Author: furia
+## Copyright (C) 2012  Sage Bionetworks <www.sagebase.org>
+## 
+## filename: restoreClassDefinition.R
+## description: restore S4 class definitions
+## author: Matthew D. Furia <matt.furia@sagebase.org>
+##
+## This file is part of the sessionTools R package.
+##
+## sessionTools is free software: provided the Funding Acknolegement
+## is maintained, you can redistribute it and/or modify it under the terms of 
+## the GNU LGPL, either version 3 of the License, or (at your option) any 
+## later version. For details visit <http://www.gnu.org/licenses/>.
+##
+## Funding Acknowledgement: 
+## The development of this software was supported by NCI Integrative Cancer 
+## Biology Program grant CA149237 and Washington State Life Science Discovery 
+## Fund Program Grant 3104672 to Sage Bionetworks.
 ###############################################################################
 
 
